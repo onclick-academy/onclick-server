51fe4acbcc85cceb8bf67ee6893069eb0c435fca
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-01-28 18:59:35 +0200

    fix[backend]: update validation for educationlevel att

--- src/validation/student.validation.ts
+++ src/validation/student.validation.ts
@@ -1,21 +1,21 @@
 import Joi from 'joi'
 
-//Student validation
 export class studentValidation {
-  //createStudent()
-  static createStudent() {
+
+  static createUpdateStudent() {
     const schema = Joi.object({
       userId: Joi.string().required(),
-      educationLevel: Joi.string().required()
+      educationLevel: Joi.valid(
+        'ELEMENTARY',
+        'MIDDLE',
+        'HIGH',
+        'COLLEGE',
+        'UNIVERSITY',
+        'MASTER',
+        'PHD')
+        .required()
     })
     return schema
   }
-  //updateStudent()
-  static updateStudent() {
-    const schema = Joi.object({
-      userId: Joi.string().required(),
-      educationLevel: Joi.string().required()
-    })
-    return schema
-  }
+
 }
