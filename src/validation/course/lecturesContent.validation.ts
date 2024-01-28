1997cff7c5f25096912c550bd9fb65a5b5a79024
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-01-28 20:36:57 +0200

    update[backend]: Update model's validtaion

--- src/validation/course/lecturesContent.validation.ts
+++ src/validation/course/lecturesContent.validation.ts
@@ -1,25 +1,28 @@
 import Joi from 'joi'
 
-//lecturesContent validation
 
 export class lecturesContentValidation {
-  //createLecturesContent()
+
   static createLecturesContent() {
     const schema = Joi.object({
+      order: Joi.number().integer().positive().required(),
       materialId: Joi.string().required(),
       lectureId: Joi.string().required(),
       content: Joi.string().required(),
-      isDeleted: Joi.boolean().default(false)
+      isDeleted: Joi.boolean().default(false),
+      deletedAt: joi.date().allow(null)
     })
     return schema
   }
-  //updateLecturesContent()
+
   static updateLecturesContent() {
     const schema = Joi.object({
+      order: Joi.number().integer().positive().required(),
       materialId: Joi.string(),
       lectureId: Joi.string(),
       content: Joi.string(),
-      isDeleted: Joi.boolean()
+      isDeleted: Joi.boolean(),
+      deletedAt: joi.date()
     })
     return schema
   }
