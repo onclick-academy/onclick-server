fadfb65887e4401c777d981236334ca4411dfdf4
Kholoud Fattem <kfattem@gmail.com>
2024-01-28 03:30:46 +0200

    feat[backend]: Add `JOI` validation for susspenstate model

---
+++ src/validation/susspendState.validation.ts
@@ -0,0 +1,25 @@
+import joi from 'joi'
+
+export class suspendStateValidation {
+  static createSuspendState() {
+    const schema = joi.object({
+      adminId: joi.string().required(),
+      userId: joi.string().required(),
+      reason: joi.string().min(20),
+      period: joi.date().required(),
+      isValid: joi.boolean().default(false)
+    })
+    return schema
+  };
+
+  static updateSuspendState() {
+    const schema = joi.object({
+      adminId: joi.string().required(),
+      userId: joi.string().required(),
+      reason: joi.string().min(20),
+      period: joi.date(),
+      isValid: joi.boolean()
+    })
+    return schema
+  };
+}
