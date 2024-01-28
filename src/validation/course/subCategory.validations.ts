a32a802bf341d6300900fff5c5e20f8c5dfd0702
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2024-01-28 18:46:07 +0200

    update[backend]: Remove the max validation for description

--- src/validation/course/subCategory.validations.ts
+++ src/validation/course/subCategory.validations.ts
@@ -1,25 +1,21 @@
 import joi from 'joi'
 
-// validation of the subCategory
-
 export class subCategoryValidation {
   static createSubCategory() {
-        //  createsubCategory()
     const schema = joi.object({
       categoryId: joi.string().required(),
       name: joi.string().required().min(6).max(255),
-      description: joi.string().required().min(6).max(255),
+      description: joi.string().required().min(6),
       isDeleted: joi.boolean().default(false)
     })
     return schema
   }
 
   static updateSubCategory() {
-        //updatesubCategory()
     const schema = joi.object({
       categoryId: joi.string(),
       name: joi.string().min(6).max(255),
-      description: joi.string().min(6).max(255),
+      description: joi.string().min(6),
       isDeleted: joi.boolean()
     })
     return schema
