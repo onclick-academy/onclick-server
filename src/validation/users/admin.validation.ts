2f98b6282fae4f6720cff6bb5f46a379ecadd9b6
Kholoud Fattem <kfattem@gmail.com>
2024-01-29 03:23:16 +0200

    update[backend]: Update model validation to have the same structure

--- src/validation/users/admin.validation.ts
+++ src/validation/users/admin.validation.ts
@@ -1,30 +1,21 @@
 import Joi from 'joi'
 
 export class adminValidation {
+  private static baseSchema = {
+    firstname: Joi.string().min(3).required(),
+    lastname: Joi.string().min(3).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    passwordConfirm: Joi.ref('password'),
+    profilePic: Joi.string(),
+    isDeleted: Joi.boolean().default(false)
+  }
+
   static createAdmin() {
-    const schema = Joi.object({
-      firstname: Joi.string().min(3).required(),
-      lastname: Joi.string().min(3).required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().required().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-      passwordConfirm: Joi.ref('password'),
-      profilePic: Joi.string(),
-      isDeleted: Joi.boolean().default(false)
-    })
-    return schema
+    return Joi.object(this.baseSchema)
   }
 
   static updateAdmin() {
-    const schema = Joi.object({
-      firstname: Joi.string(),
-      lastname: Joi.string(),
-      email: Joi.string().email(),
-      password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-      passwordConfirm: Joi.ref('password'),
-      profilePic: Joi.string(),
-      isDeleted: Joi.boolean()
-    })
-    return schema
+    return Joi.object(this.baseSchema).fork(Object.keys(this.baseSchema), schema => schema.optional())
   }
 }
-
