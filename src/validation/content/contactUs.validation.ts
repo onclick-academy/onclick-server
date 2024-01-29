08f6af2c1729c18c2bc23dbff2006f7889996ab9
Kholoud Fattem <kfattem@gmail.com>
2024-01-29 03:22:29 +0200

    update[backend]: Update model validation to have the same structure

--- src/validation/content/contactUs.validation.ts
+++ src/validation/content/contactUs.validation.ts
@@ -3,25 +3,20 @@ import joi from 'joi'
 // validation of the contactUs
 
 export class contactUsValidation {
+
+  private static baseSchema = {
+    name: joi.string().required().min(6).max(255),
+    email: joi.string().required().email(),
+    phone: joi.string().required().min(6),
+    message: joi.string().required().min(6),
+    isRead: joi.boolean().default(false)
+  }
+
   static createContactUs() {
-    const schema = joi.object({
-      name: joi.string().required().min(6).max(255),
-      email: joi.string().required().min(6).max(255),
-      phone: joi.string().required().min(6).max(255),
-      message: joi.string().required().min(6).max(255),
-      isRead: joi.boolean().default(false)
-    })
-    return schema
+    return joi.object(this.baseSchema)
   }
 
   static updateContactUs() {
-    const schema = joi.object({
-      name: joi.string().min(6).max(255),
-      email: joi.string().min(6).max(255),
-      phone: joi.string().min(6).max(255),
-      message: joi.string().min(6).max(255),
-      isRead: joi.boolean()
-    })
-    return schema
-  }
+    return joi.object(this.baseSchema).fork(Object.keys(this.baseSchema), schema => schema.optional())
+}
 }
