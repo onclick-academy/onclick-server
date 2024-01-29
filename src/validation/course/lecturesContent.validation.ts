9947ad61323673dcb480d3d90ae9eb546b6d2abb
Kholoud Fattem <kfattem@gmail.com>
2024-01-29 03:22:45 +0200

    update[backend]: Update model validation to have the same structure

--- src/validation/course/lecturesContent.validation.ts
+++ src/validation/course/lecturesContent.validation.ts
@@ -1,29 +1,25 @@
 import Joi from 'joi'
 
-
 export class lecturesContentValidation {
+  private static baseSchema = {
+    order: Joi.number().integer().positive().required(),
+    materialId: Joi.string().required(),
+    lectureId: Joi.string().required(),
+    content: Joi.string().required(),
+    isDeleted: Joi.boolean().default(false),
+    deletedAt: Joi.date().allow(null)
+  }
 
   static createLecturesContent() {
-    const schema = Joi.object({
-      order: Joi.number().integer().positive().required(),
-      materialId: Joi.string().required(),
-      lectureId: Joi.string().required(),
-      content: Joi.string().required(),
-      isDeleted: Joi.boolean().default(false),
-      deletedAt: joi.date().allow(null)
-    })
-    return schema
+    return Joi.object(this.baseSchema)
   }
 
   static updateLecturesContent() {
-    const schema = Joi.object({
-      order: Joi.number().integer().positive().required(),
-      materialId: Joi.string(),
-      lectureId: Joi.string(),
-      content: Joi.string(),
-      isDeleted: Joi.boolean(),
-      deletedAt: joi.date()
-    })
-    return schema
+    return Joi.object({
+      ...this.baseSchema,
+      materialId: Joi.string().required(),
+      lectureId: Joi.string().required(),
+      order: Joi.number().integer().positive().required()
+    }).fork(['content', 'isDeleted', 'deletedAt'], schema => schema.optional())
   }
 }
