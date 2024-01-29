3bb58e75018aa5d03bfed585a7e815c37c23bc1e
Kholoud Fattem <kfattem@gmail.com>
2024-01-29 03:23:07 +0200

    update[backend]: Update model validation to have the same structure

--- src/validation/course/subCategory.validations.ts
+++ src/validation/course/subCategory.validations.ts
@@ -1,23 +1,20 @@
 import joi from 'joi'
 
 export class subCategoryValidation {
+  private static baseSchema = {
+    categoryId: joi.string().required(),
+    name: joi.string().required().min(6).max(255),
+    description: joi.string().required().min(6),
+    isDeleted: joi.boolean().default(false)
+  }
+
   static createSubCategory() {
-    const schema = joi.object({
-      categoryId: joi.string().required(),
-      name: joi.string().required().min(6).max(255),
-      description: joi.string().required().min(6),
-      isDeleted: joi.boolean().default(false)
-    })
-    return schema
+    return joi.object(this.baseSchema)
   }
 
   static updateSubCategory() {
-    const schema = joi.object({
-      categoryId: joi.string(),
-      name: joi.string().min(6).max(255),
-      description: joi.string().min(6),
-      isDeleted: joi.boolean()
-    })
-    return schema
+    return joi
+      .object({ ...this.baseSchema, categoryId: joi.string().required() })
+      .fork(['name', 'description', 'isDeleted'], schema => schema.optional())
   }
 }
