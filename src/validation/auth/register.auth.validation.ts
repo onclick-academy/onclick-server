a9097b12fdfbe2f98d7cdaeff512b01e882d7b49
Kholoud Fattem <kfattem@gmail.com>
2024-01-29 03:22:19 +0200

    update[backend]: Update model validation to have the same structure

--- src/validation/auth/register.auth.validation.ts
+++ src/validation/auth/register.auth.validation.ts
@@ -1,39 +1,25 @@
 import joi from 'joi'
 
 export class registerValidation {
-  static createUser() {
-    const schema = joi.object({
-      fullname: joi.string().required().min(6).max(255),
-      username: joi.string().required().min(6).max(255),
-      email: joi.string().required().min(6).max(255).email(),
-      bio: joi.string().min(6).max(255),
-      password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-      passwordConfirm: joi.ref('password'),
-      phoneNum: joi.string().required().min(6).max(255),
-      profilePic: joi.string().min(6).max(255),
-      birthdate: joi.date().required(),
-      role: joi.valid('ADMIN', 'INSTRUCTOR', 'STUDENT').default('STUDENT').required(),
-      isEmailConfirm: joi.boolean().default(false)
-    })
+  private static baseSchema = {
+    fullname: joi.string().required().min(6).max(255),
+    username: joi.string().required().min(6).max(255),
+    email: joi.string().required().email(),
+    bio: joi.string().min(6).max(255).optional(),
+    password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    passwordConfirm: joi.ref('password'),
+    phoneNum: joi.string().required().min(6).max(255),
+    profilePic: joi.string().min(6).max(255),
+    birthdate: joi.date().required(),
+    role: joi.valid('ADMIN', 'INSTRUCTOR', 'STUDENT').default('STUDENT').required(),
+    isEmailConfirm: joi.boolean().default(false)
+  }
 
-    return schema
+  static createUser() {
+    return joi.object(this.baseSchema)
   }
 
   static updateUser() {
-    const schema = joi.object({
-      fullname: joi.string().min(6).max(255),
-      username: joi.string().min(6).max(255),
-      email: joi.string().min(6).max(255).email(),
-      bio: joi.string().min(6).max(255),
-      password: joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-      passwordConfirm: joi.ref('password'),
-      phoneNum: joi.string().min(6).max(255),
-      birthdate: joi.date(),
-      profilePic: joi.string().min(6).max(255),
-      role: joi.valid('ADMIN', 'INSTRUCTOR', 'STUDENT'),
-      isEmailConfirm: joi.boolean()
-    })
-
-    return schema
+    return joi.object(this.baseSchema).fork(Object.keys(this.baseSchema), schema => schema.optional())
   }
 }
