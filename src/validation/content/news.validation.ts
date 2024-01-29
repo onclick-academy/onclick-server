ea448ac9889b7d43d440a9b3f8282fe537948bdc
Kholoud Fattem <kfattem@gmail.com>
2024-01-29 03:22:34 +0200

    update[backend]: Update model validation to have the same structure

--- src/validation/content/news.validation.ts
+++ src/validation/content/news.validation.ts
@@ -1,29 +1,22 @@
 import joi from 'joi'
 
 export class newsValidation {
+  private static baseSchema = {
+    adminId: joi.string().required(),
+    title: joi.string().required().min(6).max(255),
+    description: joi.string().required().min(6),
+    images: joi.object(),
+    cover: joi.string().required(),
+    isDeleted: joi.boolean().default(false),
+    isAvailable: joi.boolean().default(true)
+  }
   static createNews() {
-    const schema = joi.object({
-      adminId: joi.string().required(),
-      title: joi.string().required().min(6).max(255),
-      description: joi.string().required().min(6),
-      images: joi.object(),
-      cover: joi.string().required(),
-      isDeleted: joi.boolean().default(false),
-      isAvailable: joi.boolean().default(true)
-    })
-    return schema
+    return joi.object(this.baseSchema)
   }
 
   static updateNews() {
-    const schema = joi.object({
-      adminId: joi.string().required(),
-      title: joi.string().min(6).max(255),
-      description: joi.string().min(6),
-      images: joi.object(),
-      cover: joi.string(),
-      isDeleted: joi.boolean(),
-      isAvailable: joi.boolean()
-    })
-    return schema
+    return joi
+      .object({ ...this.baseSchema, adminId: joi.string().required() })
+      .fork(['title', 'description', 'images', 'cover', 'isDeleted', 'isAvailable'], schema => schema.optional())
   }
 }
