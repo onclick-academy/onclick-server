9fd47863e67fa6ccf65a81e7d9f5c39f82e21acf
Kholoud Fattem <kfattem@gmail.com>
2024-02-07 08:33:02 +0200

    Update[backend]: Update sending email to send for Admin too

--- src/utilities/email.ts
+++ src/utilities/email.ts
@@ -9,13 +9,49 @@ export default async function sendEmail(req: Request, res: Response, user: any, 
     const secret = process.env.JWT_SECRET_KEY
     const token = createToken({ email: user.email, id: user.id }, secret, { expiresIn: '15m' })
 
-    const resetToken = await prisma.resetToken.create({
-      data: {
-        token,
-        userId: user.id,
-        expiresAt: new Date(Date.now() + 15 * 60 * 1000)
+    let resetToken = null as any
+
+    if (type === 'Reset') {
+      if (user.fullName) {
+        resetToken = await prisma.resetToken.create({
+          data: {
+            token,
+            userId: user.id,
+            expiresAt: new Date(Date.now() + 15 * 60 * 1000)
+          }
+        })
+      } else {
+        resetToken = await prisma.resetToken.create({
+          data: {
+            token,
+            adminId: user.id,
+            expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000)
+          }
+        })
       }
-    })
+    }
+
+    let confirmToken = null as any
+
+    if (!type || type === 'Confirm') {
+      if (user.fullName) {
+        confirmToken = await prisma.confirmToken.create({
+          data: {
+            token,
+            userId: user.id,
+            expiresAt: new Date(Date.now() + 15 * 60 * 1000)
+          }
+        })
+      } else {
+        confirmToken = await prisma.confirmToken.create({
+          data: {
+            token,
+            adminId: user.id,
+            expiresAt: new Date(Date.now() + 24 * 60 * 60 * 1000)
+          }
+        })
+      }
+    }
 
     const url =
       type === 'Confirm'
@@ -38,13 +74,17 @@ export default async function sendEmail(req: Request, res: Response, user: any, 
         type === 'Reset'
           ? `     <div>
                 <h1>Reset Password</h1>
-                <p> dear ${user.username}, You requested to RESET your password if it wasn't you!, please ignore this email,
+                <p> dear ${
+                  user.username ? user.username : user.fullName ? user.fullName : 'ADMIN'
+                }, You requested to RESET your password if it wasn't you!, please ignore this email,
                 otherwise, Click on the link below to reset your password please <br> ${url} <br> ps. this link is VALID for 15m.</p>
               </div>
               `
           : ` <div>
               <h1>Email Confirmation</h1>
-              <p> dear ${user.username}, You have successfully registered to our platform, but before you can start using it, you need to confirm your email :) if it wasn't you!, please ignore this email,
+              <p> dear ${
+                user.username ? user.username : user.fullName ? user.fullName : 'Admin'
+              }, You have successfully registered to our platform, but before you can start using it, you need to confirm your email :) if it wasn't you!, please ignore this email,
               <br> Click on the link below to confirm your email please <br> ${url} <br> ps. this link is VALID for 1 Day.</p>
           </div>
       `
@@ -56,7 +96,7 @@ export default async function sendEmail(req: Request, res: Response, user: any, 
         return res.status(500).json({ error: err.message })
       } else {
         console.log('email sent')
-        return res.status(200).json({ data: 'email sent', reset_token: resetToken })
+        return res.status(200).json({ data: 'email sent', reset_token: resetToken, confirm_token: confirmToken })
       }
     })
   } catch (error) {
