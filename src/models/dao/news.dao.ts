ebfeca5a036bdb2f535183ef2eeeb9973a0287d8
MominMorgan <mominmorgan10@gmail.com>
2024-02-07 23:38:45 +0200

    feat[backend]: Add News dao

---
+++ src/models/dao/news.dao.ts
@@ -0,0 +1,55 @@
+import prisma from '../prisma/prisma-client'
+
+export class NewsDao {
+  createNews = async (newsDto: NewsDtoI) => {
+    const news = await prisma.news.create({
+      data: {
+        id: newsDto.id,
+        title: newsDto.title,
+        subtitle: newsDto.subtitle,
+        adminId: newsDto.adminId,
+        description: newsDto.description,
+        images: newsDto.images,
+        isAvailable: newsDto.isAvailable,
+        cover: newsDto.cover
+      }
+    })
+    return news
+  }
+
+  getNews = async (id: string) => {
+    const news = await prisma.news.findFirst({
+      where: {
+        id: id
+      }
+    })
+    return news
+  }
+
+  updateNews = async (newsDto: NewsDtoI) => {
+    const news = await prisma.news.update({
+      where: {
+        id: newsDto.id
+      },
+      data: {
+        title: newsDto.title,
+        subtitle: newsDto.subtitle,
+        adminId: newsDto.adminId,
+        description: newsDto.description,
+        images: newsDto.images,
+        isAvailable: newsDto.isAvailable,
+        cover: newsDto.cover
+      }
+    })
+    return news
+  }
+
+  deleteNews = async (id: string) => {
+    const news = await prisma.news.delete({
+      where: {
+        id: id
+      }
+    })
+    return news
+  }
+}
