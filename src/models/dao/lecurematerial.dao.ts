07e58b7800e43ce034adc210e1a19428146f9396
MominMorgan <mominmorgan10@gmail.com>
2024-02-07 23:43:56 +0200

    feat[backend]: Add Lecturematerial dao

---
+++ src/models/dao/lecurematerial.dao.ts
@@ -0,0 +1,49 @@
+import { lectureMaterialDto } from '@models/dto/lectureMaterial.dto'
+import prisma from '../prisma/prisma-client'
+
+export class LectureMaterialDao {
+  createLectureMaterial = async (lectureMaterialDto: lectureMaterialI) => {
+    const lectureMaterial = await prisma.lecturesMaterials.create({
+      data: {
+        id: lectureMaterialDto.id,
+        title: lectureMaterialDto.title,
+        description: lectureMaterialDto.description
+      }
+    })
+    return lectureMaterial
+  }
+
+  getLectureMaterial = async (id: string) => {
+    const lectureMaterial = await prisma.lecturesMaterials.findFirst({
+      where: {
+        id: id,
+        isDeleted: false
+      }
+    })
+    return lectureMaterial
+  }
+
+  updateLectureMaterial = async (lectureMaterialDto: lectureMaterialI) => {
+    const lectureMaterial = await prisma.lecturesMaterials.update({
+      where: {
+        id: lectureMaterialDto.id,
+        isDeleted: false
+      },
+      data: {
+        title: lectureMaterialDto.title,
+        description: lectureMaterialDto.description
+      }
+    })
+    return lectureMaterial
+  }
+
+  deleteLectureMaterial = async (id: string) => {
+    const lectureMaterial = await prisma.lecturesMaterials.delete({
+      where: {
+        id: id,
+        isDeleted: false
+      }
+    })
+    return lectureMaterial
+  }
+}
