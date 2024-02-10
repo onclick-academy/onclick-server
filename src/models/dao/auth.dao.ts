f7a29c9b63096b4a06d35725a98b52f2440dcdaf
Kholoud Fattem <kfattem@gmail.com>
2024-02-10 05:49:52 +0200

    fix[backend]: Fix some issues in auth and student daos

--- src/models/dao/auth.dao.ts
+++ src/models/dao/auth.dao.ts
@@ -1,6 +1,73 @@
-import prisma from '@models/prisma/prisma-client'
-import { comparePassword } from '@utilities/hash'
-import { UserTokenI, GlobalUserI, loginDtoI } from 'types/user.interface'
+import prisma from '../prisma/prisma-client'
+import { comparePassword } from '../../utilities/hash'
+import { UserTokenI } from "../../types/user.interface"
+import { GENDER , ROLE, EDUCATION_LEVEL} from '@prisma/client'
+
+interface GlobalUserI {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+
+  fullName: string
+  username: string
+  email: string
+  password: string
+  bio: string
+  phoneNum: string
+  profilePic: string
+  birthDate: Date
+  gender: GENDER
+  role: ROLE
+  educationLevel: EDUCATION_LEVEL
+  isEmailConfirm: boolean
+  isDeleted: boolean
+  deletedAt: Date
+  isAvailable: boolean
+}
+interface UserDtoI {
+  id: string
+  fullName: string
+  username: string
+  email: string
+  bio: string
+  password: string
+  phoneNum: string
+  profilePic?: string
+  birthDate: Date
+  gender: GENDER
+  role: ROLE
+  educationLevel: EDUCATION_LEVEL
+  isEmailConfirm: boolean
+  isDeleted?: boolean
+  isRememberMe?: boolean
+}
+
+interface UserUpdateI {
+  id: string
+  fullName?: string
+  username?: string
+  email?: string
+  bio?: string
+  password?: string
+  phoneNum?: string
+  profilePic?: string
+  birthDate?: Date
+  gender?: GENDER
+  role?: ROLE
+  educationLevel?: EDUCATION_LEVEL
+  isEmailConfirm?: boolean
+  isDeleted?: boolean
+}
+
+interface loginDtoI {
+  email?: string
+  password?: string
+  username?: string
+  role?: string
+  id?: string
+  isAvailable?: boolean
+  isRememberMe?: boolean
+}
 
 export class AuthDao {
   login = async (userDto: loginDtoI) => {
@@ -40,12 +107,12 @@ export class AuthDao {
   }
 
   getUserByEmail = async (userDto: { email: string }) => {
-    const user = await prisma.user.findUnique({
+    const user = (await prisma.user.findUnique({
       where: {
         email: userDto.email,
         isDeleted: false
       }
-    })
+    })) as GlobalUserI
     if (!user) throw new Error('Email is not found please register')
     return user
   }
