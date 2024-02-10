b8515223ee938edd3354df9cbb90fd49c05ddb7b
Kholoud Fattem <kfattem@gmail.com>
2024-02-10 02:51:13 +0200

    feat[backend]: Add Topic dto, dao, joi validation, interfaces, controllers and route to the main index - working

--- src/middlewares/validation/course/topic.validation.ts
+++ src/middlewares/validation/course/topic.validation.ts
@@ -1,6 +1,6 @@
 import joi from 'joi'
 
-export class topicValidation {
+export class TopicValidation {
   private static baseSchema = {
     title: joi.string().required(),
     isDeleted: joi.boolean().default(false),
@@ -8,14 +8,11 @@ export class topicValidation {
     courses: joi.array().items(joi.string()).allow(null)
   }
 
-  static createTopic() {
-    return joi.object(this.baseSchema)
+  static createTopic(topicDto: TopicDtoI) {
+    return joi.object(this.baseSchema).validateAsync(topicDto)
   }
 
   static updateTopic() {
-    return joi
-      .object(this.baseSchema)
-      .fork(['title', 'isDeleted', 'deletedAt'], schema => schema.optional())
+    return joi.object(this.baseSchema).fork(['title', 'isDeleted', 'deletedAt'], schema => schema.optional())
   }
 }
-
