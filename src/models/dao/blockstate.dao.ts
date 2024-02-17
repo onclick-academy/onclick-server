940516d3915bc2d02634bb3b62b69fabbfe4b55e
Omar Salah <omar.salah1597@gmail.com>
2024-02-17 12:25:44 +0200

    Update blockstate.dao.ts

--- src/models/dao/blockstate.dao.ts
+++ src/models/dao/blockstate.dao.ts
@@ -4,14 +4,7 @@ import prisma from '../prisma/prisma-client'
 export class BlockStateDao {
         createBlockState = async (blockStateDto: BlockStateI) => {
         const blockState = await prisma.blockState.create({
-        data: {
-                id: blockStateDto.id,
-                userId: blockStateDto.UserDtoId,
-                adminId: blockStateDto.adminId,
-                state: blockStateDto.state,
-                reason: blockStateDto.reason,
-                period: blockStateDto.period
-        }
+        data: blockStateDto
         })
         return blockState
         }
@@ -30,13 +23,7 @@ export class BlockStateDao {
         where: {
                 id: blockStateDto.id
         },
-        data: {
-                userId: blockStateDto.UserDtoId,
-                adminId: blockStateDto.adminId,
-                state: blockStateDto.state,
-                reason: blockStateDto.reason,
-                period: blockStateDto.period
-        }
+        data: blockStateDto
         })
         return blockState
         }
