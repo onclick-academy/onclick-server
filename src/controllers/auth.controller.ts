27fe3c9abdea03e4ce76c40d3765002cc849728f
0marSalah <omar.salah1597@gmail.com>
2024-02-18 16:27:43 +0200

    fix[backend]: remove admin case from register

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,36 +2,35 @@ import { expiredPeriod } from '../..' // refer to index.ts on the root directory
 import { Request, Response } from 'express'
 import joi from 'joi'
 
-import { RegisterDto } from '@models/dto/register.dto'
 import { AuthDao } from '@dao/auth.dao'
 import { UserDao } from '@dao/user.dao'
 import { LoginValidation } from '@validation/auth/login.auth.validation'
-import { registerValidation } from '@validation/auth/register.auth.validation'
+import { RegisterValidation } from '@validation/auth/register.auth.validation'
 import { LoginDto } from '@dto/login.dto'
 import prisma from '@models/prisma/prisma-client'
 import { createToken } from '@utilities/token'
 import sendEmail from '@utilities/email'
-import { AdminDao } from '@models/dao/admin.dao'
+import { UserRequest } from '../types/user.interface'
+import { UserDto } from '@models/dto/user.dto'
 
 export class AuthController {
-    static register = async (req: any, res: Response) => {
-        const userDto = new RegisterDto(req.body)
+    static register = async (req: UserRequest | any, res: Response) => {
+        const userDto = new UserDto(req.body)
+        const userDao = new UserDao()
 
         if (req.file) {
             userDto.profilePic = req.file.path
         }
 
-        const userDao = new UserDao()
-
         try {
-            const isExist = await prisma.admin.findUnique({
+            const isExist = await prisma.user.findUnique({
                 where: {
                     email: userDto.email
                 }
             })
             if (isExist) return res.status(400).json({ error: 'Email is already in use', status: 'failed' })
 
-            const { error } = await registerValidation.createUser(userDto)
+            const { error } = await RegisterValidation.createUser(userDto)
             if (error) {
                 return res.status(400).json({ error: 'Error when creating user' })
             }
@@ -45,20 +44,23 @@ export class AuthController {
                 expiresIn: expiredPeriod.refreshToken
             })
 
-            await this.sendConfirmationEmail(req, res)
-
-            req.user = {
-                id: newUser.id,
-                role: newUser.role,
-                email: newUser.email
-            }
-
-            return res.status(200).json({
+            res.status(200).json({
                 data: newUser,
                 accessToken: accessToken,
                 refreshToken: userDto.isRememberMe ? refreshToken : null,
                 status: 'success'
             })
+
+            req.user = {
+                id: newUser.id,
+                role: newUser.role,
+                email: newUser.email,
+                accessToken: accessToken,
+                refreshToken: refreshToken
+            }
+
+            await this.sendConfirmationEmail(req, res) // make it last step for its Time complexity
+            return
         } catch (error: any) {
             if (
                 error.message.includes('Email is already in use') ||
@@ -86,19 +88,11 @@ export class AuthController {
             }
 
             let user = await authDao.getUserByEmail({ email: userEmail })
+            if (!user) throw new Error('User not found')
 
-            if (!user) {
-                const admin = await prisma.admin.findUnique({
-                    where: {
-                        email: userEmail
-                    }
-                })
-                if (!admin) throw new Error('User not found')
-                await sendEmail(admin, 'CONFIRM')
-            }
             await sendEmail(user as any, 'CONFIRM')
         } catch (error: any) {
-            if (error.message.includes('Email')) {
+            if (error.message.includes('Email') || error.message.includes('User')) {
                 return res.status(400).json({ error: error.message })
             }
             return res.status(500).json({ error: error.message })
@@ -124,37 +118,11 @@ export class AuthController {
             const userDao = new UserDao()
             const updatedUser = await userDao.updateUser({ id: user.id, isEmailConfirm: true })
 
+            res.redirect('http://localhost:3000/login')
             return res.status(200).json({ data: updatedUser, status: 'success' })
         } catch (error: any) {
             console.log(error.message)
-            return res.status(500).json({ error: error.message })
-        }
-    }
-
-    static emailAdminConfirmation = async (req: Request, res: Response) => {
-        const { adminId, token } = req.params
-        try {
-            const admin = await prisma.admin.findUnique({
-                where: {
-                    id: adminId
-                },
-                include: {
-                    confirmToken: true
-                }
-            })
-            if (!admin) throw new Error('Admin not found')
-            if (!admin.confirmToken) throw new Error('Token not found')
-            if (admin.confirmToken.expiresAt < new Date()) throw new Error('Token has expired')
-            if (admin.confirmToken.token !== token) throw new Error('Token is not valid')
-            if (admin.isEmailConfirm) throw new Error('Email is already confirmed')
-
-            const adminDao = new AdminDao()
-            const updatedAdmin = await adminDao.updateAdmin({ id: admin.id, isEmailConfirm: true })
-
-            return res.status(200).json({ data: updatedAdmin, status: 'success' })
-        } catch (error: any) {
-            console.log(error.message)
-            return res.status(500).json({ error: error.message })
+            return res.status(500).json({ error: error.message, status: 'error' })
         }
     }
 
