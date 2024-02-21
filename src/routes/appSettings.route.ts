099b24a6847505376ec5cbd8338b9df7c6dea6c1
Kholoud Fattem <kfattem@gmail.com>
2024-02-21 02:35:52 +0200

    feat[backend]: Implement settings routes

---
+++ src/routes/appSettings.route.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { AppSettingsController } from '@controllers/appSettings.controller'
+
+const router = express.Router();
+
+router
+    .route('/')
+    .post(AppSettingsController.createAppSettings)
+    .get(AppSettingsController.getAppSettings)
+    .put(AppSettingsController.updateAppSettings);
+
+export default router;
