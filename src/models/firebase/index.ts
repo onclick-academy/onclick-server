2bf24c268f97a7514c55ebecdc7538f4b9ab9c41
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2024-03-22 21:23:33 +0200

    SERVICE_ACCOUNT env

--- src/models/firebase/index.ts
+++ src/models/firebase/index.ts
@@ -1,6 +1,6 @@
 import * as admin from 'firebase-admin'
 
-const serviceAccount = require('../../../admins.json')
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
