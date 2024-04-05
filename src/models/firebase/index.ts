23b466a91a440767a849adffc4031ce9208b5187
Ahmed Hany <39633124+dev-ahmedhany@users.noreply.github.com>
2024-04-05 02:16:20 +0200

    fix[backend]: fix build

--- src/models/firebase/index.ts
+++ src/models/firebase/index.ts
@@ -1,7 +1,6 @@
 import * as admin from 'firebase-admin'
 
 const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
- origin
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
